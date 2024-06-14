4520767546eb50114e6b4ac20216d599ba2bc8ce
Oscar <ozkin1vs@hotmail.com>
2024-06-14 16:34:25 -0500

    iniciando reto foro alura

---
+++ src/main/java/com/aluraRetoForo/RetoAluraForoApplication.java
@@ -0,0 +1,13 @@
+package com.aluraRetoForo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAluraForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoAluraForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraRetoForo/RetoAluraForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraRetoForo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoAluraForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
